8b786c7cba52f8ebfc4d17611ffdaabc37a568a1
Jens Skott <jens.skott@gmail.com>
2017-11-20 14:51:36 +0100

    Most of structure done

---
+++ api/service.go
@@ -0,0 +1,13 @@
+package api
+
+import "github.com/jensskott/spiderman/parser"
+
+var definition *parser.Definition
+
+func CreateService(def *parser.Definition) {
+
+}
+
+func (def parser.Definition) CreateTaskDefinition() {
+
+}
