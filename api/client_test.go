73957469f2a67bf9f4f34ae50286fd13931703af
Jens Skott <jens.skott@gmail.com>
2017-11-24 19:36:17 +0100

    Updated client test with mock key and secret

--- api/client_test.go
+++ api/client_test.go
@@ -8,19 +8,19 @@ import (
 )
 
 func TestEc2Client(t *testing.T) {
-	c := Ec2Client("us-west-1")
+	c := Ec2Client("us-west-1", "test", "test")
 	assert.IsType(t, Ec2Implementation{}, c)
 	assert.IsType(t, session.Session{}, *c.Session)
 }
 
 func TestElbV2Client(t *testing.T) {
-	c := ElbV2Client("us-west-1")
+	c := ElbV2Client("us-west-1", "test", "test")
 	assert.IsType(t, ElbV2Implementation{}, c)
 	assert.IsType(t, session.Session{}, *c.Session)
 }
 
 func TestEcsClient(t *testing.T) {
-	c := EcsClient("us-west-1")
+	c := EcsClient("us-west-1", "test", "test")
 	assert.IsType(t, EcsImplementation{}, c)
 	assert.IsType(t, session.Session{}, *c.Session)
 }
